c7bd30437731ecbc32b39c46357ab7f841cf32ce
Joe Dietrich <joe@joedietrich.net>
2021-08-01 22:28:05 -0400

    Done!

--- src/index.js
+++ src/index.js
@@ -4,6 +4,29 @@ async function app() {
   const quoteData = await fetch('http://localhost:3000/quotes?_embed=likes').then(res => res.json());
   const quoteForm = document.getElementById('new-quote-form');
   const quoteList = document.getElementById('quote-list');
+  const sortButton = document.getElementById('sort-button');
+  let isSorted = false;
+
+  sortButton.addEventListener('click', () => {
+    if (isSorted) {
+      quoteData.sort((first, second) => first.id - second.id);
+      console.table(quoteData)
+      quoteList.innerHTML = '';
+      renderQuotes(quoteData);
+    } else {
+      quoteData.sort((first, second) => {
+        const auth1 = first.author.toUpperCase();
+        const auth2 = second.author.toUpperCase();
+        return auth1 < auth2 ? -1 : auth1 > auth2 ? 1 : 0;
+      });
+      console.table(quoteData)
+      quoteList.innerHTML = '';
+      renderQuotes(quoteData);
+    }
+    isSorted = !isSorted;
+    sortButton.classList.toggle('btn-dark');
+    sortButton.classList.toggle('btn-light');
+  })
 
   quoteForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -41,11 +64,15 @@ async function app() {
     quoteFooter.classList.add('blockquote-footer');
     quoteFooter.innerText = quoteDatum.author;
 
+    const buttonGroup = document.createElement('div');
+    buttonGroup.classList.add('btn-group');
+
     const likeButton = document.createElement('button');
-    likeButton.classList.add('btn-success');
+    likeButton.classList.add('btn', 'btn-success');
     const likeSpan = document.createElement('span');
     likeSpan.innerText = quoteDatum.likes.length;
-    likeButton.append('Likes: ', likeSpan);
+    likeSpan.classList.add('badge', 'badge-danger', 'badge-pill')
+    likeButton.append('Likes ', likeSpan);
     likeButton.addEventListener('click', () => {
       const timeLiked = Math.floor(Date.now() / 1000);
       fetch('http://localhost:3000/likes', {
@@ -64,8 +91,16 @@ async function app() {
         })
     })
 
+    const editButton = document.createElement('button');
+    editButton.classList.add('btn', 'btn-primary');
+    editButton.innerText = 'Edit';
+    editButton.addEventListener('click', () => {
+      const editCard = renderEditQuote(quoteDatum);
+      quoteList.replaceChild(editCard, quoteCard);
+    })
+
     const deleteButton = document.createElement('button');
-    deleteButton.classList.add('btn-danger');
+    deleteButton.classList.add('btn', 'btn-danger');
     deleteButton.innerText = 'Delete';
     deleteButton.addEventListener('click', () => {
       fetch(`http://localhost:3000/quotes/${quoteDatum.id}`, {
@@ -75,18 +110,98 @@ async function app() {
       })
     })
 
+    buttonGroup.append(likeButton, editButton, deleteButton);
+
     blockQuote.append(
       quoteText,
       quoteFooter,
       document.createElement('br'),
-      likeButton,
-      deleteButton
+      buttonGroup
     );
     quoteCard.appendChild(blockQuote);
-    quoteList.appendChild(quoteCard);
+    return quoteCard;
+  }
+
+  function renderEditQuote(quoteDatum) {
+    const quoteCard = document.createElement('li');
+    quoteCard.classList.add('card', 'quote-card', 'edit-card');
+
+    const quoteEditForm = document.createElement('form');
+
+    const quoteGroup = document.createElement('div');
+    quoteGroup.classList.add('form-group');
+    const quoteLabel = document.createElement('label');
+    quoteLabel.innerText = 'Edit Author';
+    quoteLabel.for = `quote-${quoteDatum.id}-quote`;
+    const quoteText = document.createElement('input');
+    quoteText.value = quoteDatum.quote;
+    quoteText.type = 'text';
+    quoteText.id = `quote-${quoteDatum.id}-quote`;
+    quoteText.name = `quote-${quoteDatum.id}-quote`;
+    quoteText.classList.add('form-control');
+    quoteGroup.append(quoteLabel, quoteText);
+
+    const authorGroup = document.createElement('div');
+    authorGroup.classList.add('form-group');
+    const authorLabel = document.createElement('label');
+    authorLabel.innerText = 'Edit Author';
+    authorLabel.for = `quote-${quoteDatum.id}-author`;
+    const authorText = document.createElement('input');
+    authorText.value = quoteDatum.author;
+    authorText.type = 'text';
+    authorText.id = `quote-${quoteDatum.id}-author`;
+    authorText.name = `quote-${quoteDatum.id}-author`;
+    authorText.classList.add('form-control');
+    authorGroup.append(authorLabel, authorText);
+
+    const buttonGroup = document.createElement('div');
+    buttonGroup.classList.add('btn-group');
+
+    const saveButton = document.createElement('button');
+    saveButton.classList.add('btn', 'btn-primary');
+    saveButton.innerText = 'Save';
+    saveButton.type = 'submit';
+    saveButton.addEventListener('click', (e) => {
+      e.preventDefault();
+      fetch(`http://localhost:3000/quotes/${quoteDatum.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8'
+        },
+        body: JSON.stringify({
+          quote: quoteText.value,
+          author: authorText.value,
+        })
+      }).then(res => res.json())
+        .then((data) => {
+          quoteDatum.author = data.author;
+          quoteDatum.quote = data.quote;
+          const viewCard = renderQuote(quoteDatum);
+          quoteList.replaceChild(viewCard, quoteCard);
+        })
+    })
+
+    const cancelButton = document.createElement('button');
+    cancelButton.classList.add('btn', 'btn-secondary');
+    cancelButton.innerText = 'Cancel';
+    cancelButton.type = 'button';
+    cancelButton.addEventListener('click', () => {
+      const viewCard = renderQuote(quoteDatum);
+      quoteList.replaceChild(viewCard, quoteCard);
+    })
+
+    buttonGroup.append(saveButton, cancelButton)
+
+    quoteEditForm.append(
+      quoteGroup,
+      authorGroup,
+      buttonGroup
+    );
+    quoteCard.appendChild(quoteEditForm);
+    return quoteCard;
   }
 
   function renderQuotes(quoteData) {
-    quoteData.forEach(quoteDatum => renderQuote(quoteDatum));
+    quoteData.forEach(quoteDatum => quoteList.appendChild(renderQuote(quoteDatum)));
   }
 }
